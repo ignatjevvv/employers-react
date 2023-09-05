30123592e6da1237e6084f91704f16ec74d45fa5
Константин Игнатьев <ignatjev@obe.local>
2023-09-05 19:58:59 +0300

    Add key for function rendering list employers

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -8,9 +8,9 @@ import EmployersAddForm from '../employers-add-form/employers-add-form';
 function App() {
 
     const data = [
-        {name: 'John C.', salary: 800, increase: true},
-        {name: 'Alex M.', salary: 3000, increase: false},
-        {name: 'Carl W.', salary: 5000, increase: false},
+        {name: 'John C.', salary: 800, increase: false, id: 1},
+        {name: 'Alex M.', salary: 3000, increase: true, id: 2},
+        {name: 'Carl W.', salary: 5000, increase: false, id: 3},
     ];
 
     return (
